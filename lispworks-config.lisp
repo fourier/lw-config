39c0634796a222ed000493c03b600e330560420f
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2015-05-15 22:36:50 +0200

    Updated bindings and renamed old dvorak bindings

--- lispworks-config.lisp
+++ lispworks-config.lisp
@@ -1,5 +1,9 @@
 ;; -*- Mode: lisp; -*-
 (in-package "CL-USER")
+
+;; load all LispWorks patches
+(load-all-patches)
+
 ;;; The following lines added by ql:add-to-init-file:
 #-quicklisp
 (let ((quicklisp-init (merge-pathnames ".quicklisp/setup.lisp"
@@ -16,44 +20,27 @@
 ;; #= ## syntax to denote shared structures including circles.
 (setf *print-circle* t)
 
-;; Bind Ctrl-Up/Down to previous/next commands in Listener
-(editor:bind-key "History Next" "Control-Down" :mode "Execute")
-(editor:bind-key "History Previous" "Control-Up" :mode "Execute")
-;; Same for Cmd-UP/Down 
-(editor:bind-key "History Next" "Hyper-Down" :mode "Execute")
-(editor:bind-key "History Previous" "Hyper-Up" :mode "Execute")
-
-;; Enter will indent new line
-(editor:bind-key "Indent New Line" #\Return :mode "Lisp")
+;; do not produce error when unbalanced right paren inserted
+(setf system:*right-paren-whitespace* :warn)
 
 ;; Indent as in Emacs
 (editor:setup-indent "if" 2 4 4)
 
-;; per-word movements
-(editor:bind-key "Forward Word" "Control-Right" :global)
-(editor:bind-key "Backward Word" "Control-Left" :global)
-
-(editor:bind-key "Forward Word" "Hyper-Right" :global)
-(editor:bind-key "Backward Word" "Hyper-Left" :global)
-
-;; Cmd-x as extended command ?? doesn't work
-(editor:bind-key "Extended Command" "Hyper-x" :global :mac)
-
 ;; Run GUI inspect when called from REPL
 (setf *inspect-through-gui* t)
 
-(editor:bind-key "Function Documentation" "Hyper-F1" :global)
-(editor:bind-key "Show Documentation" "Meta-F1" :global)
-;;(editor:bind-key "Function Arglist" "Meta-=" :global)
-;;(editor:bind-key "Function Argument List" "Meta-A" :global)
-
 (ql:quickload "cl-fad")
 
-(let* ((ctrl-u-binds-file "ctrl-u-mac-binds.lisp")
-       (ctrl-u-binds-path (cl-fad:merge-pathnames-as-file (cl-fad:pathname-directory-pathname *load-truename*) ctrl-u-binds-file)))
-  (load ctrl-u-binds-path))
-                            
-;(load "ctrl-u-mac-binds.lisp")
+(defun load-config-file (filename)
+  (let ((file-full-path (cl-fad:merge-pathnames-as-file (cl-fad:pathname-directory-pathname *load-truename*) filename)))
+    (load file-full-path)))
+  
+(require "delete-selection")
+(editor:delete-selection-mode-command)
+
+(load-config-file "editor-extensions.lisp")
+(load-config-file "dvorak-binds.lisp")
+(load-config-file "other-binds.lisp")
 
 
 (format *standard-output* "~%Press Cmd+F1 to show Hyperspec for symbol~%")
---
+++ other-binds.lisp
@@ -0,0 +1,65 @@
+;; -*- Mode: lisp; -*-
+(in-package "CL-USER")
+
+
+;; Bind Ctrl-Up/Down to previous/next commands in Listener
+(editor:bind-key "History Next" "Control-Down" :mode "Execute")
+(editor:bind-key "History Previous" "Control-Up" :mode "Execute")
+;; Same for Cmd-UP/Down 
+(editor:bind-key "History Next" "Hyper-Down" :mode "Execute")
+(editor:bind-key "History Previous" "Hyper-Up" :mode "Execute")
+
+;; Enter will indent new line
+(editor:bind-key "Indent New Line" #\Return :mode "Lisp")
+
+;; per-word movements
+(editor:bind-key "Forward Word" "Control-Right" :global)
+(editor:bind-key "Backward Word" "Control-Left" :global)
+
+(editor:bind-key "Forward Word" "Hyper-Right" :global)
+(editor:bind-key "Backward Word" "Hyper-Left" :global)
+
+(editor:bind-key "Function Documentation" "Hyper-F1" :global)
+(editor:bind-key "Show Documentation" "Meta-F1" :global)
+;;(editor:bind-key "Function Arglist" "Meta-=" :global)
+;;(editor:bind-key "Function Argument List" "Meta-A" :global)
+
+;; selection keys to emulate Mac-emulation mode
+(require "mac-mode")
+(require "selection-mode")
+(editor:bind-key "Backward Character Extending Selection" "Shift-Left" :global)
+(editor:bind-key "Forward Character Extending Selection"  "Shift-Right" :global)
+(editor:bind-key "Previous Line Extending Selection"      "Shift-Up" :global)
+(editor:bind-key "Next Line Extending Selection"          "Shift-Down" :global)
+
+(editor:bind-key "Backward Word Extending Selection"      "Meta-Shift-Left" :global)
+(editor:bind-key "Forward Word Extending Selection"       "Meta-Shift-Right" :global)
+
+(editor:bind-key "Backward Word Extending Selection"      "Control-Shift-Left" :global)
+(editor:bind-key "Forward Word Extending Selection"       "Control-Shift-Right" :global)
+
+(editor:bind-key "Backward Form Extending Selection"      "Meta-Control-Shift-Left" :global)
+(editor:bind-key "Forward Form Extending Selection"       "Meta-Control-Shift-Right" :global)
+(editor:bind-key "End of Defun Extending Selection"       "Meta-Control-Shift-Next" :global)
+(editor:bind-key "Beginning of Defun Extending Selection" "Meta-Control-Shift-Prior" :global)
+(editor:bind-key "Backward Up List Extending Selection"   "Meta-Control-Shift-Up" :global)
+(editor:bind-key "Down List Extending Selection"          "Meta-Control-Shift-Down" :global)
+
+;; Control-Backspace as usual
+(editor:bind-key "Kill Previous Word" "Control-Backspace" :global)
+
+;; emacs-like comment hotkey
+(editor:bind-key "Comment Do What I Mean" "Meta-;" :global)
+
+;; Ctrl-Up/Down move one paragraph up/down
+(editor:bind-key "Forward Paragraph" "Control-Down" :global)
+(editor:bind-key "Backward Paragraph" "Control-Up" :global)
+
+;; Cmd-Up/Down move one sentence up/down
+(editor:bind-key "Forward Sentence" "Hyper-Down" :global)
+(editor:bind-key "Backward Sentence" "Hyper-Up" :global)
+
+;; Goto Line
+(editor:bind-key "Goto Line" #("Control-u" "g") :global)
+(editor:bind-key "Delete Indentation" #("Control-u" "j") :global)
+
